dfdb56e4c0c4e67d5bdaadbe4228def65d6de507
GlebN_K <nizhnik.gleb@gmail.com>
2023-09-26 17:44:47 +0300

    HW-8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,16 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: any, action: ActionType): any => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            return action.payload === 'up'
+                ? [...state].sort((a, b) => a.age - b.age)
+                : [...state].sort((a, b) => b.age - a.age)
         }
         case 'check': {
 
-            return state // need to fix
+            return [...state].filter(user => user.age >= 18) // need to fix
         }
         default:
             return state
