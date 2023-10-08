4cf24c84c7432fadd68565d72cc5e217a688d9b8
GlebN_K <nizhnik.gleb@gmail.com>
2023-10-08 15:44:47 +0300

    HW-10

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,11 @@
 import s from './Loader.module.css'
+import * as React from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 
-export const Loader = () => <div className={s.loader}/>
+// export const Loader = () => <div className={s.loader}/>
+export const Loader = () => <div className={s.loader}>
+    <Box sx={{ display: 'flex' }}>
+        <CircularProgress />
+    </Box>
+</div>
